90447ba5b10afb1bce6ad2162f20f35a748b9b39
justinelut <justinequartz@gmail.com>
2022-07-06 14:26:58 +0000

    Fixed next.config image error

--- next.config.js
+++ next.config.js
@@ -1,15 +1,34 @@
 require('dotenv').config();
 const { sizes } = require('./blocks/Image/sizes');
+const withPWA = require("next-pwa");
 
-module.exports = {
+module.exports = withPWA({
   publicRuntimeConfig: {
     SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   },
   images: {
     domains: [
       'theverge.co.ke',
+      "robohash.org", 
+      "res.cloudinary.com"
       // Your domain(s) here
     ],
     deviceSizes: sizes,
   },
-};
+  // module.exports = {
+  i18n: {
+    locales: ["en", "my"],
+    defaultLocale: "en",
+  },
+  reactStrictMode: true,
+  // swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },
+  pwa: {
+    dest: "public",
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  // };
+});
